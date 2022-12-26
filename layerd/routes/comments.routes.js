f925b753ba3f6682251c4efd74bb87457540be89
kyuhyung <vision8love@gmail.com>
2022-12-26 16:46:48 +0900

    댓글-유저 연결

--- layerd/routes/comments.routes.js
+++ layerd/routes/comments.routes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middlewares/auth');
 
 const CommentsController = require('../controllers/comments.controller');
 const commentsController = new CommentsController();
 
-router.post('/:postId', commentsController.createComment);
-router.put('/comment-update/:commentId', commentsController.updateComment);
-router.delete('/comment-delete/:commentId', commentsController.deleteComment);
+router.post('/:postId', auth.isLoggedIn, commentsController.createComment);
+router.put(
+  '/comment-update/:commentId',
+  auth.isLoggedIn,
+  commentsController.updateComment,
+);
+router.delete(
+  '/comment-delete/:commentId',
+  auth.isLoggedIn,
+  commentsController.deleteComment,
+);
 
 module.exports = router;
