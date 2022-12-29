951632d1afc3694c304570edfbdce82be8c9d1a2
akalavigne <115855341+akalavigne@users.noreply.github.com>
2022-12-29 18:08:26 +0900

    최종 머지

--- layerd/controllers/social.controller.js
+++ layerd/controllers/social.controller.js
@@ -1,39 +1,21 @@
-// const { Users } = require('../../models');
-// const passport = require('passport');
-// const KakaoStrategy = require('passport-kakao').Strategy;
+const KakaoUsersService = require('../services/social.service');
+const logger = require('../../config/loggers');
 
-// class SocialLoginController {
-//   login = async (req, res, next) => {
-//     const { accessToken, profile, done } = passport.authenticate(
-//       new KakaoStrategy(),
-//     );
-//     console.log(accessToken);
-//   };
-// }
+class KakaoUsersController {
+  kakaoUsersService = new KakaoUsersService();
 
-// module.exports = SocialLoginController;
+  kakaoLogin = async (req, res, next) => {
+    try {
+      const headers = req.headers['authorizaion'];
+      const kakaoToken = headers.split(' ')[1];
 
-// try {
-//     const exUser = await Users.findOne({
-//       // 카카오 플랫폼에서 로그인 했고 & snsId필드에 카카오 아이디가 일치할경우
-//       where: { snsId: profile.id, provider: 'kakao' },
-//     });
-//     // 이미 가입된 카카오 프로필이면 성공
-//     if (exUser) {
-//       done(null, exUser); // 로그인 인증 완료
-//     } else {
-//       // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
-//       const newUser = await Users.create({
-//         writer: kakao_account.profile.nickname,
-//         snsId: profile.id,
-//         provider: 'kakao',
-//       });
+      const accessToken = await kakaoUsersService.kakaoLogin(kakaoToken);
+      return res.status(200).json({ accessToken: accessToken });
+    } catch (error) {
+      logger.error(`status code :, ${error.status}, error message : ${error}`);
+      return res.status(error.status).json({ error: error.message });
+    }
+  };
+}
 
-//       done(null, newUser); // 회원가입하고 로그인 인증 완료
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     done(error);
-//   }
-
-//   console.log(accessToken);
+module.exports = KakaoUsersController;
--- layerd/routes/auth.routes.js
+++ layerd/routes/auth.routes.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const auth = require('../../middlewares/auth');
+// const KakaoUsersController = require('../controllers/social.controller');
+// const kakaoUsersController = new KakaoUsersController();
 
 router.get('/kakao', passport.authenticate('kakao-login'));
 router.get(
   '/kakao/callback',
   passport.authenticate('kakao-login', {
-    failureRedirect: '/',
+    failureRedirect: 'https://brunch-clone-codding-fe.vercel.app/',
     session: false,
   }),
   (req, res) => {
-    res.redirect('/');
+    res.redirect('https://brunch-clone-codding-fe.vercel.app/');
   },
 );
 
